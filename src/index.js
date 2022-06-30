12cc743b62711b6c59144cb5783bc695d4d57e2b
Vas Vasa <dzho.ker.700@gmail.com>
2022-06-30 14:23:27 +0300

    docs: add complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(a) {
+    let reverseNum = a + ''
+    let NoMinus1 = reverseNum.replace(/[^a-zа-яё0-9\s]/gi, ' ');
+    let NoMinus  = (Number(NoMinus1.split('').reverse().join(''))) 
+    return NoMinus
 }
+
